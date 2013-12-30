bb9fac7b5f82fe6e1e320690cd6f687b166f9c7f
syhw <gabriel.synnaeve@gmail.com>
2013-12-30 17:49:14 +0100

    put the result in the adaptor grammars qsub

--- launch_adaptor.sh
+++ launch_adaptor.sh
@@ -1,1 +1,5 @@
 ./py-cfg_grammarsamples/py-cfg -n 100 -G ${1}.wlt -A ${1}.prs -F ${1}.trace -x 1 -E -d 101 -a 0.0001 -b 10000 -e 1 -f 1 -g 100 -h 0.001 -R -1 -Y "cat > ${1}.samples" -P ${1}.lt < ${2}.ylt
+. /etc/profile.d/modules.sh
+module load python-anaconda/2.7.5
+python scripts/trees-words.py -c "^Word" -i "^_d" < ${1}.prs > ${1}.seg
+python scripts/eval.py -g ${1}.gold < ${1}.seg
