ff21312f443726970cb9cdcf3fff91c63cf580d4
syhw <gabriel.synnaeve@gmail.com>
2014-01-24 18:49:29 +0100

    better plotting

--- src/plot_AGs_results.py
+++ src/plot_AGs_results.py
@@ -11,7 +11,7 @@ N_MONTHS = EAGE-SAGE+1
 #TYPES = ["basic", "single-context", "topics"]
 #TYPES = ["basic", "topics"]
 TYPES = ["basic", "single-context"]
-ITERS = [100, 500]
+ITERS = [10, 100, 500]
 PREFIX = ""
 #PREFIX = "old_naima_XPs/"
 TAKE_MAX_SCORE = True # in case of several results
@@ -98,4 +98,27 @@ plt.xlabel('months')
 plt.ylabel('token f-score')
 plt.legend([l for l in results.iterkeys()], loc='best', ncol=4)
 plt.setp(ax.get_legend().get_texts(), fontsize=12)
-plt.savefig('progress.png')
+plt.savefig('progress_ages.png')
+
+for month in xrange(SAGE, EAGE+1):
+    y_pos = [0.5]
+    scores = []
+    conds = []
+    for cond, a in results.iteritems():
+        score = a[month-SAGE]
+        if score > 0:
+            y_pos.append(y_pos[-1] + 1)
+            scores.append(score)
+            conds.append(cond)
+    y_pos = y_pos[:-1]
+    fig = plt.figure(figsize=(9, len(y_pos)), dpi=300)
+    ax = plt.gca()
+    ax.set_ylim([0, len(y_pos)+1])
+    tmp = zip(y_pos, scores, conds, ['g' for tmp_i in range(len(y_pos))])
+    tmp = map(lambda (y, s, cond, color): (y, s, cond, 'b') if 't_' in cond or 'd_' in cond else (y, s, cond, color), tmp)
+    y_pos, scores, conds, colors = zip(*tmp)
+    plt.barh(y_pos, scores, color=colors, alpha=0.4)
+    plt.yticks(map(lambda x: x+0.5, y_pos), conds)
+    plt.xlabel('token f-score')
+    #plt.title('')
+    plt.savefig('histogram_' + str(SAGE_XPS) + 'to' + str(month) + 'm.png')
